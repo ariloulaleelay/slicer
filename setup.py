4dc6169f2d5b9a65cb39350b94e657a9c1bc9bf5
Andrey Proskurnev <aproskurnev@nic.ru>
2015-12-22 15:52:44 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='slicer',
+    version='0.1.0',
+    description='Hive cube slicer server',
+    author='Andrey Proskurnev',
+    author_email='andrey@proskurnev.ru',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        'pyhocon==0.3.11',
+        'sqlalchemy',
+        'cherrypy',
+        'pyhive[hive]',
+    ]
+)
